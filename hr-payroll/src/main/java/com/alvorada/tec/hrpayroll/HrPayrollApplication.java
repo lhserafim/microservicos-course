aa488ca956ebb75089441bbabd4276abc39f55c0
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-04-23 10:22:02 -0300

    18. Configurando clientes Eureka

--- hr-payroll/src/main/java/com/alvorada/tec/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/alvorada/tec/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,13 @@ package com.alvorada.tec.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "hr-worker") // indicar de quem o projeto é cliente
+@EnableEurekaClient
+// O Eureka já tem o balanceamento de carga implementado. Nâo preciso mais do ribbon
+// @RibbonClient(name = "hr-worker") // indicar de quem o projeto é cliente
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
