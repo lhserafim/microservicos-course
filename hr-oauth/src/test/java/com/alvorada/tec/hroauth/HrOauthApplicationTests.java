f80047b042378d3efde1cb83ffa04ec8a46c02f9
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-04-27 08:31:53 -0300

    36. Criando projeto hr-oauth

---
+++ hr-oauth/src/test/java/com/alvorada/tec/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvorada.tec.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
